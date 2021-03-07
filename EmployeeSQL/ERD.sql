a210b28c5bcaaa006a0d0e28899021f880416567
Sugeily <sugeilymg@gmail.com>
2021-03-06 21:56:13 -0600

    tables

---
+++ EmployeeSQL/ERD.sql
@@ -0,0 +1,67 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/jarqCZ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Departments" (
+    "dept_no" string   NOT NULL,
+    "dept_name" string   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" string   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "emp_no" int   NOT NULL,
+    "dept_no" string   NOT NULL,
+    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" string   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    "sex" string   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" string   NOT NULL,
+    "title" string   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Salaries" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
